b65e61c550c957b6aa5590ca77c8a731a413413d
Selim <selim@MacBook-Pro-de-bouhassatine.local>
2021-02-06 11:30:42 +0100

    Fix function insert comment and add function toggle

--- PEXTRANET_01_CODE/index.php
+++ PEXTRANET_01_CODE/index.php
@@ -15,12 +15,27 @@ if(isset($_GET['action'])) {
     }
     else if($_GET['action'] == 'addCommentaire') {
         if(isset($_GET['id_acteur'])) {
-            PostCommentaire($_SESSION['id_user'], $_GET['id_acteur'], $_SESSION['username'], $_POST['text']);
+            if($_POST['text'] != "") {
+                PostCommentaire($_SESSION['id_user'], $_GET['id_acteur'], 
+                $_SESSION['username'], $_POST['text']);
+            } 
+            else {
+                header('Location: index.php?action=PageActeur&id_acteur='. $_GET['id_acteur']);
+            }
+        } 
+
+    }
+    else if($_GET['action'] == 'addLike/Dislike') {
+        if(isset($_GET['id_acteur'])) {
+            if($_GET['type'] == 1) {
+                toggle_like($_GET['id_acteur'], $_SESSION['id_user']);
+            }
+            else if($_GET['type'] == 2) {
+                toggle_dislike($_GET['id_acteur'], $_SESSION['id_user']);
+            }
         }
     }
 }
 else {
     PageHome();
 }
-
-
