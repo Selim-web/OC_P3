2be171ad44cc80b36f45df85dcb793550740cd12
Selim <selim@MacBook-Pro-de-bouhassatine.local>
2021-02-06 11:28:18 +0100

    Add new function in model

--- PEXTRANET_01_CODE/model/modele.php
+++ PEXTRANET_01_CODE/model/modele.php
@@ -91,9 +91,69 @@ function addCommentaire($id_user,$id_acteur,$username,$text)
 function getNbrLikeByActeur ($id_acteur)
 {
     $bdd = bddConnect();
-    // finir la fonction
+    
+    $likes = $bdd->prepare('SELECT * FROM likes WHERE id_acteur = ?');
+    $likes->execute(array($_GET['id_acteur']));
+    $likes = $likes->rowCount();
+
+    return $likes;
 
 }
 
+function getNbrDislikeByActeur ($id_acteur)
+{
+    $bdd = bddConnect();
 
+    $dislikes = $bdd->prepare('SELECT * FROM dislikes WHERE id_acteur = ?');
+    $dislikes->execute(array($_GET['id_acteur']));
+    $dislikes = $dislikes->rowCount();
+
+    return $dislikes;
+}
+
+function check_like($id_acteur, $id_user) {
+    
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+    $check_like = $bdd->prepare('SELECT * FROM likes WHERE id_acteur = ? AND id_user = ?');
+    $check_like->execute(array($id_acteur,$id_user));
+
+    return $check_like->rowCount();
+}
+
+function check_dislike($id_acteur, $id_user) {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+
+    $check_dislike = $bdd->prepare('SELECT * FROM dislikes WHERE id_acteur = ? AND id_user = ?');
+    $check_dislike->execute(array($id_acteur,$id_user));
+
+    return $check_dislike->rowCount();
+}
+
+function insert_like($id_acteur, $id_user) {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+
+    $like = $bdd->prepare('INSERT INTO likes (id_acteur,id_user) VALUE (?,?)');
+    $like->execute(array($id_acteur,$id_user));    
+}
+
+function insert_dislike($id_acteur, $id_user) {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+
+    $dislike = $bdd->prepare('INSERT INTO dislikes (id_acteur,id_user) VALUE (?,?)');
+    $dislike->execute(array($id_acteur,$id_user));    
+}
+
+function delete_like($id_acteur, $id_user) {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+
+    $delete_like = $bdd->prepare('DELETE FROM likes WHERE id_acteur = ? AND id_user = ?');
+    $delete_like->execute(array($id_acteur,$id_user));
+}
+
+function delete_dislike($id_acteur, $id_user) {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=8889;dbname=GBAF','root', 'root'); 
+
+    $delete_dislike = $bdd->prepare('DELETE FROM dislikes WHERE id_acteur = ? AND id_user = ?');
+    $delete_dislike->execute(array($id_acteur,$id_user));
+}
 ?>
